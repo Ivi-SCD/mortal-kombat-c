e7c245494a7fa53314d1be1b01485a649b47605c
Ivi <ivipnascimento@hotmail.com>
2023-03-15 23:38:02 -0300

    Inicializando caracteres e exibindo a lista de personagens

--- main.c
+++ main.c
@@ -1,32 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-//inicialização do struct dos lutadores
-typedef struct{
-    char nome[30];
-    float ataque;
-    float defesa;
-    float vida;
-}Lutadores;
 
+/*
+ * Inicialização do struct dos lutadores
+ *
+ * */
+
+
+typedef struct{
+    char name[30];
+    float attack;
+    float defense;
+    float life;
+} Players;
+
+void initializeCharacters(Players* characters);
+void showCharacterList(Players* characters, int choose[3]);
 
 int main(void) {
-    srand(time(NULL));
-//atribuição do vetor de personagens
-    Lutadores personagens[30];
 
-    int vitorias = 0;
-//inicialização dos atributos
-    for(int i = 0; i < 30; i++){
-        personagens[i].ataque = rand()%10;
-        personagens[i].defesa = rand()%10;
-        personagens[i].vida = 100;
-    }
-//teste da inicialização
-    //for(int i = 0; i < 30; i++){
-    //printf("%f\n %f\n %f\n", personagens[i].ataque, personagens[i].ataque, personagens[i].vida);
-    //}
+    srand(time(NULL));
+
+    // Atribuição do vetor de personagens
+    int chooseCharacters[3];
+    Players characters[30];
+
+    int wins = 0;
+
+    initializeCharacters(characters);
+    showCharacterList(characters,  chooseCharacters);
 
     return 0;
+}
+
+void initializeCharacters(Players* characters) {
+    // Inicialização dos atributos
+    for(int i = 0; i < 30; i++){
+        characters[i].attack = rand()%10;
+        characters[i].defense = rand()%10;
+        characters[i].life = 100;
+    }
+}
+
+void showCharacterList(Players * characters, int choose[3] ) {
+    //Tabela de Personagens
+    printf("|++---- Tabela de Personagens ----++|\n");
+    printf("     Attack | Defense | Life\n");
+    for(int i = 0; i < 30; i++){
+        printf("  Personagem(%d):%.2f %.2f %.2f\n", i, characters[i].attack, characters[i].defense, characters[i].life);
+    }
+    printf("Escolha seus personagens: \n");
+    scanf("%d %d %d", &choose[0], &choose[1], &choose[2]);
 }
